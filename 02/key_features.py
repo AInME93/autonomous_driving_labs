71d9a96efc6325d164842d28d6bf54476ac37743
AInMe93 <imransaid247@gmail.com>
2023-04-24 16:19:31 -0500

    Plot vehicle motion

--- 02/key_features.py
+++ 02/key_features.py
@@ -2,13 +2,11 @@ import os
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+import time
+from utils import get_calib, set_np_arr
 
-dir_path = "C:\\Users\\imran\\Documents\\My Docs\\SLU\\Msc AI\\SEM 3\\CSCI 5930\\Assignments\\2011_09_26_drive_0001_sync"
-img_path = os.path.join(dir_path, "image_02\data\\0000000000.png")
-
-img = cv.imread(img_path, cv.IMREAD_GRAYSCALE) # `<opencv_root>/samples/data/blox.jpg`
-
-EXTENSIONS = ['png', 'jpg']
+# dir_path = "C:\\Users\\imran\\Documents\\My Docs\\SLU\\Msc AI\\SEM 3\\CSCI 5930\\Assignments\\2011_09_26_drive_0001_sync"
+# img_path = os.path.join(dir_path, "image_02\data\\0000000000.png")
 
 def list_images(dir_path):
 
@@ -19,10 +17,6 @@ def list_images(dir_path):
     files = [f for f in files if f.endswith('.png')]
 
     return files
-
-
-# def pos():
-
 
 def fast_feature_extract(img, threshold):
     # Initiate FAST object with default values
@@ -48,11 +42,10 @@ def fast_feature_extract(img, threshold):
     img3 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
     cv.imwrite('fast_false.png', img3)
 
-
 def orb_feature_extract(img, nfeatures=3000):
 
     # Initiate ORB detector
-    orb = cv.ORB_create(nfeatures=30000)
+    orb = cv.ORB_create(nfeatures)
 
     # compute the descriptors with ORB
     kp, des = orb.detectAndCompute(img, None)
@@ -62,45 +55,170 @@ def orb_feature_extract(img, nfeatures=3000):
 
     return img2, kp, des
 
+def log_keypoints(keypoints, kp_set=set(), exclude_kp=[]):
 
-def main():
+    for x in keypoints:
+        if x not in exclude_kp:
+            kp_set.add(x)
+            
+    return kp_set
 
-    # specify work directories
-    dir_path = "C:\\Users\\imran\\Documents\\My Docs\\SLU\\Msc AI\\SEM 3\\CSCI 5930\\Assignments\\2011_09_26_drive_0001_sync\\image_02\\data"
-    output_dir = os.path.join(dir_path, "output")
+def save_keypoint_idx(keypoint_idx, idx, file_name="output.txt"):
 
-    # get names of files
-    files = list_images(dir_path)
+    # Open file for writing
+    with open("output.txt", "a") as file:
+        # Write the elements of the list to a line separated by commas
+        line = f"{idx}. {','.join(str(e) for e in keypoint_idx)}"
+        file.write(line + "\n")
 
-    #read images
-    img1 = cv.imread(os.path.join(dir_path, files[0]), cv.IMREAD_GRAYSCALE)
-    img2 = cv.imread(os.path.join(dir_path, files[4]), cv.IMREAD_GRAYSCALE)
-
-    # get keypoints and descriptors
-    _, kp1, des1 = orb_feature_extract(img1)
-    _, kp2, des2 = orb_feature_extract(img2)
-
-
-    # Create a brute-force matcher and match the descriptors
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
+def filter_distance(matches):
 
     # Apply distance ratio test to filter out ambiguous matches
     matches = sorted(matches, key = lambda x:x.distance)
 
     # good_matches = []
+
     # for m in matches:
-    #     if m.distance < 0.7 * matches[0].distance:
+    #     if m.distance < 5.0 * matches[0].distance:
     #         good_matches.append(m)
 
+    return matches
+    # return good_matches
+
+def display_matches(img1, kp1, img2, kp2, matches, pause_length=1000):
     # Draw the matched keypoints on the images
-    match_img = cv.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    match_img = cv.drawMatches(img1, kp1, img2, kp2, matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     match_img = cv.resize(match_img, (1280,360))
 
     # Display the result
     cv.imshow('Matches', match_img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()    
+    cv.waitKey(pause_length)
+    cv.destroyAllWindows()
+
+
+def feature_match_points(dir_path, files, first_idx, idx_increment):
+
+    
+    #read first image
+    img1 = cv.imread(os.path.join(dir_path, files[first_idx]), cv.IMREAD_GRAYSCALE)
+
+    # get keypoints and descriptors
+    _, kp1, des1 = orb_feature_extract(img1)
+
+    img2 = cv.imread(os.path.join(dir_path, files[first_idx+idx_increment]), cv.IMREAD_GRAYSCALE)
+
+    _, kp2, des2 = orb_feature_extract(img2)
+
+    # Create a brute-force matcher and match the descriptors
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+    matches = filter_distance(matches)
+
+    # get indices and values of matched keypoints in both images
+    matches_img1 = [kp1[m.queryIdx].pt for m in matches[0:8]]
+    matches_img2 = [kp2[m.trainIdx].pt for m in matches[0:8]]
+
+    # display_matches(img1, kp1, img2, kp2, matches)
+
+    return matches_img1, matches_img2
+
+def find_R_t(coord1:list, coord2:list, K):
+
+    kp_coord_1 = np.int32(coord1)
+    kp_coord_2 = np.int32(coord2)
+    print(kp_coord_1, kp_coord_2)
+    if kp_coord_1 is None or kp_coord_2 is None:
+        print("MAJOR ERROR")
+
+    F, mask = cv.findFundamentalMat(kp_coord_1, kp_coord_2, cv.FM_8POINT)
+    # # Print the fundamental matrix and the number of inliers
+    # print("Fundamental matrix:\n", F)
+    # print("Number of inliers:", np.sum(mask))
+    E, _ = cv.findEssentialMat(kp_coord_1, kp_coord_2, K, cv.RANSAC)
+
+    print(E)
+    pts, R, t, mask = cv.recoverPose(E, kp_coord_1, kp_coord_2, K)
+
+    return pts, R, t, mask
+
+# def plotTrajectory():
+
+
+def main():
+
+    list_matched1, list_matched2 = [] , []
+    camera_positions = []
+
+    # specify work directories
+    dir_path = "..\\..\\2011_09_26_drive_0001_sync\\image_02\\data"
+    calib_path = "..\\..\\2011_09_26_drive_0001_sync\\calib_cam_to_cam.txt"
+
+    calib = get_calib(calib_path)
+
+    # get names of files
+    files = list_images(dir_path)
+    
+
+    # Get camera intrinsic matrix
+    # [ fx_00   0   cx_00 ]
+    # [   0   fy_00 cy_00 ]
+    # [   0     0     1   ]
+
+    K = set_np_arr((3,3), calib["K_00"].strip().split(" "))
+
+    idx_increment = 1
+
+    idx = 0
+    while idx < len(files)-idx_increment and idx != 30:
+        try:
+            # adjust idx_increment (last argument) to compare with next frame or second frame after and so on
+            matches1, matches2 = feature_match_points(dir_path, files, idx, idx_increment) 
+            list_matched1.append(matches1)
+            list_matched2.append(matches2)
+        except IndexError:
+            print('End of files')
+            break
+
+        idx += idx_increment
+
+
+    # origin = np.array([0, 0, 0])
+    cur_position = np.zeros((3,1))
+    cur_rotation = np.eye(3)
+
+    camera_positions.append(cur_position)
+
+
+    # return Rotation and translation  matrices
+    for i in range(len(list_matched1)-1):
+
+        print(i)
+
+        pts, R, t, mask = find_R_t(list_matched1[i], list_matched2[i], K)
+
+        cur_position = R.dot(cur_position) + t
+
+        print('shape r.current',R.dot(cur_position).shape)
+        print('shape t',t.shape)
+        camera_positions.append(cur_position)
+        
+        # print("Camera positions: ",camera_positions[0])
+        # break
+
+    plots = np.array(camera_positions).T
+    print((plots[0][0], plots[0][1]))
+
+    # Plot the trajectory
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(plots[0][0], plots[0][1], '-b')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    # ax.set_zlabel('Z')
+    ax.set_title('Vehicle Trajectory')
+    plt.show()
+
+
 
 if __name__ == "__main__":
     main()
