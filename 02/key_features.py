b098e7e10f5dd46341a302dd890cc2ca09209c2e
AInMe93 <imransaid247@gmail.com>
2023-05-07 23:43:32 -0500

    feature matcher changes

--- 02/key_features.py
+++ 02/key_features.py
@@ -4,6 +4,7 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 import time
 from utils import get_calib, set_np_arr
+from statistics import mean
 
 # dir_path = "C:\\Users\\imran\\Documents\\My Docs\\SLU\\Msc AI\\SEM 3\\CSCI 5930\\Assignments\\2011_09_26_drive_0001_sync"
 # img_path = os.path.join(dir_path, "image_02\data\\0000000000.png")
@@ -42,7 +43,7 @@ def fast_feature_extract(img, threshold):
     img3 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
     cv.imwrite('fast_false.png', img3)
 
-def orb_feature_extract(img, nfeatures=3000):
+def orb_feature_extract(img, nfeatures=500):
 
     # Initiate ORB detector
     orb = cv.ORB_create(nfeatures)
@@ -74,16 +75,36 @@ def save_keypoint_idx(keypoint_idx, idx, file_name="output.txt"):
 def filter_distance(matches):
 
     # Apply distance ratio test to filter out ambiguous matches
-    matches = sorted(matches, key = lambda x:x.distance)
+    # matches = sorted(matches, key = lambda x:x.distance)
 
+    good_matches = []
+    # print(matches)
+    for m, n in matches:
+        if not m.distance < 0.8 * n.distance:
+            good_matches.append(m)
+
+    total_matches = np.size(matches)
+    
+    # # set the minimum distance as the minimum distance we found between key points
+    # min_dist = matches[0].distance
+    
+    # # set the maximum distance as the maximum distance we found between key points
+    # max_dist = matches[total_matches - 1].distance
+    
+    # # set threshold to find good matches
     # good_matches = []
+    # if min_dist < 20:  # You can try different threshold
+    #     min_dist = 20
 
-    # for m in matches:
-    #     if m.distance < 5.0 * matches[0].distance:
-    #         good_matches.append(m)
+    # for i in range(0, total_matches - 1):
+    #     if matches[i].distance >= min_dist and matches[i].distance <= max_dist * 0.66:
+    #         good_matches.append(matches[i])
 
-    return matches
-    # return good_matches
+    # # sort good matches
+    # good_matches = sorted(good_matches, key=lambda x: x.distance)
+    
+        # return matches
+    return good_matches
 
 def display_matches(img1, kp1, img2, kp2, matches, pause_length=1000):
     # Draw the matched keypoints on the images
@@ -95,10 +116,8 @@ def display_matches(img1, kp1, img2, kp2, matches, pause_length=1000):
     cv.waitKey(pause_length)
     cv.destroyAllWindows()
 
-
 def feature_match_points(dir_path, files, first_idx, idx_increment):
 
-    
     #read first image
     img1 = cv.imread(os.path.join(dir_path, files[first_idx]), cv.IMREAD_GRAYSCALE)
 
@@ -110,13 +129,14 @@ def feature_match_points(dir_path, files, first_idx, idx_increment):
     _, kp2, des2 = orb_feature_extract(img2)
 
     # Create a brute-force matcher and match the descriptors
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
+    # bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1,des2,k=2)
     matches = filter_distance(matches)
 
     # get indices and values of matched keypoints in both images
-    matches_img1 = [kp1[m.queryIdx].pt for m in matches[0:8]]
-    matches_img2 = [kp2[m.trainIdx].pt for m in matches[0:8]]
+    matches_img1 = [kp1[m.queryIdx].pt for m in matches]
+    matches_img2 = [kp2[m.trainIdx].pt for m in matches]
 
     # display_matches(img1, kp1, img2, kp2, matches)
 
@@ -127,8 +147,9 @@ def find_R_t(coord1:list, coord2:list, K):
     kp_coord_1 = np.int32(coord1)
     kp_coord_2 = np.int32(coord2)
     print(kp_coord_1, kp_coord_2)
+
     if kp_coord_1 is None or kp_coord_2 is None:
-        print("MAJOR ERROR")
+        raise Exception("MAJOR ERROR")
 
     F, mask = cv.findFundamentalMat(kp_coord_1, kp_coord_2, cv.FM_8POINT)
     # # Print the fundamental matrix and the number of inliers
@@ -166,10 +187,10 @@ def main():
 
     K = set_np_arr((3,3), calib["K_00"].strip().split(" "))
 
-    idx_increment = 1
+    idx_increment = 2
 
     idx = 0
-    while idx < len(files)-idx_increment and idx != 30:
+    while idx < len(files)-idx_increment:
         try:
             # adjust idx_increment (last argument) to compare with next frame or second frame after and so on
             matches1, matches2 = feature_match_points(dir_path, files, idx, idx_increment) 
@@ -192,21 +213,19 @@ def main():
     # return Rotation and translation  matrices
     for i in range(len(list_matched1)-1):
 
-        print(i)
-
         pts, R, t, mask = find_R_t(list_matched1[i], list_matched2[i], K)
 
         cur_position = R.dot(cur_position) + t
 
-        print('shape r.current',R.dot(cur_position).shape)
-        print('shape t',t.shape)
+        # print('shape r.current',R.dot(cur_position).shape)
+        # print('shape t',t.shape)
         camera_positions.append(cur_position)
         
         # print("Camera positions: ",camera_positions[0])
         # break
 
     plots = np.array(camera_positions).T
-    print((plots[0][0], plots[0][1]))
+    # print((plots[0][0], plots[0][1]))
 
     # Plot the trajectory
     fig = plt.figure()
